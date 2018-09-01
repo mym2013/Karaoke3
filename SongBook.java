c1eb68dfa6179d5fc4b0de5e9b9f9f8c4fb471ef
mym2013 <torresdelfiero@gmail.com>
2018-09-01 16:53:16 +0000

    telling that from line 28 the code must be ceched// indicando que desde la lúnea 28 el codigo debe ser cached

--- SongBook.java
+++ SongBook.java
@@ -23,13 +23,13 @@ public class SongBook {
   
   // FIXME:  This should be cached!
   private Map<String, List<Song>> byArtist() {
-		//
+		// indicar que el atajo de la línea 28<> es sólo valido para java 7 hacia arriba
+	// just to point out that this shorthand  on line 28, is only available on java  7 and higher		
 		
     Map<String, List<Song>> byArtist = new HashMap<>();
     for (Song song : mSongs) {
       List<Song> artistSongs = byArtist.get(song.getArtist());
-	// indicar que el atajo de la línea 28<> es sólo valido para java 7 hacia arriba
-	// just to point out that this shorthand  on line 28, is only available on java  7 and higher		
+
 /**** 	Nota importante!!  lo que hace este if, es que verifica en el Map, mediante el ingreso de la key del Map, tomada de una lista de canciones, verificando y asociando  en caso de encontrar al artista, a un lote de posibles canciones (o al menos una), sino encuentra el artista, es decir retorna un null en el if, crea de inmediato una lista tipo arreglo, que luego completa ingresando en el Map( que se llama byArtist en este codigo) asi qeda el nuevo mapa actualizado y devuelve entonces un mapa como fué indicado en el comienzo del desarrollo de esta función (un poco complejo pero funciona)  	****/	
 /**** 	Important Note: this if statement, is checking using the keyMap, getting from a List of <Songs> if it is possible to find out the artist, if it so, it is gonna return the Map, no changes, but if is not, has to create a new list, and then has tu put on the map the artist and the artist song, and then returning the Map refreshed	****/	
   if (artistSongs == null) {
